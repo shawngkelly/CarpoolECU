fcb0bf655792c28c3d41661744f4383ac8f172fa
shawngkelly <shawngkelly@hotmail.com>
2016-09-22 13:10:57 -0400

    Added Simple Rider Class
    Added Simple Rider Class

---
+++ src/main/java/com/carpool/model/Rider.java
@@ -0,0 +1,22 @@
+package com.carpool.model;
+
+/**
+ * Created by shawngkelly on 9/9/16.
+ */
+
+
+public class Rider {
+
+  private int RiderId;
+
+
+  public Rider(){
+
+  }
+
+  public int getRiderId() {
+    return RiderId;
+  }
+
+
+  }
